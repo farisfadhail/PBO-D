3d8a00f13f05b1e04046ab78ec879fea653e7eb0
Faris Fadhail <farisfadhail@gmail.com>
2024-05-27 19:25:51 +0700

    Add project files.

---
+++ PBO-D/Program.cs
@@ -0,0 +1,10 @@
+﻿class Program
+{
+    public static void main(String[] args)
+    {
+        Console.WriteLine("Absensi Video Github :");
+
+        // Tuliskan nama dan nim kalian di bawah ini
+        // Console.WriteLine("Faris Fadhail | 2124*******");
+    }
+}
